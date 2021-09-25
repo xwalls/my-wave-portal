950d643ae8402e44283be6529d2270c49a8af108
Xavier Paredes <jxavierpp1@gmail.com>
2021-09-25 12:17:55 -0700

    🚧 Wait until our contract is officially deployed

--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,7 @@
 const main = async () => {
     const WaveContractFactory = await hre.ethers.getContractFactory('WavePortal');
     const WaveContract = await WaveContractFactory.deploy();
-    
+    await waveContract.deployed();
 }
 
 const runMain = async () => {}
