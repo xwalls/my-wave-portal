22cc05e3c43749df20c9f02a17067b28ac51278b
Xavier Paredes <jxavierpp1@gmail.com>
2021-09-25 12:18:42 -0700

    🚧 Print address of the deployed contract

--- scripts/run.js
+++ scripts/run.js
@@ -2,6 +2,7 @@ const main = async () => {
     const WaveContractFactory = await hre.ethers.getContractFactory('WavePortal');
     const WaveContract = await WaveContractFactory.deploy();
     await waveContract.deployed();
+    console.log("Contract deployed to:", waveContract.address);
 }
 
 const runMain = async () => {}
