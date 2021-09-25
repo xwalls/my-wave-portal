6ab4a9b744bd2b7a82073a2c104bf721767664b0
Xavier Paredes <jxavierpp1@gmail.com>
2021-09-25 12:17:10 -0700

    🚧  Create a local Ethereum network

--- scripts/run.js
+++ scripts/run.js
@@ -1,5 +1,7 @@
 const main = async () => {
     const WaveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+    const WaveContract = await WaveContractFactory.deploy();
+    
 }
 
 const runMain = async () => {}
