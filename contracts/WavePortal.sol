5519c87eff080d31ed9bbd912fde41fe071ace20
Xavier Paredes <jxavierpp1@gmail.com>
2021-09-25 17:33:30 -0700

    ✨ Define totalWaves variable

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("Im a mexican smart contract");
     }
