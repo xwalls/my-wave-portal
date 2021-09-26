5869b84a046f760371d81da30ca22afc173085e1
Xavier Paredes <jxavierpp1@gmail.com>
2021-09-25 17:50:39 -0700

    ✨ Make it multiplayer!

--- scripts/run.js
+++ scripts/run.js
@@ -9,8 +9,13 @@ const main = async () => {
 
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
-    
+
     let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    waveTxn = await waveContract.connect(randoPerson).wave();
     await waveTxn.wait();
 
     waveCount = await waveContract.getTotalWaves();
