2b515a81a64ac784d2f197bca2b9688ffbf827ff
Xavier Paredes <jxavierpp1@gmail.com>
2021-09-25 20:12:42 -0700

    ✨ Initialize deploy.js script

---
+++ scripts/deploy.js
@@ -0,0 +1,13 @@
+const main = async () => { };
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+runMain();
