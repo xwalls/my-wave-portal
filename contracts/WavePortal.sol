e7bffca8922a0657bc2db791fe6f6b70a26fa0df
Xavier Paredes <jxavierpp1@gmail.com>
2021-09-25 17:34:50 -0700

    ✨ Add getTotalWaves function

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -15,4 +15,9 @@ contract WavePortal {
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
     }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
 }
