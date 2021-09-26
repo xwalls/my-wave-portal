70aef4f9037c6b978aa94f693f8e9c0ed4886468
Xavier Paredes <jxavierpp1@gmail.com>
2021-09-25 20:18:31 -0700

    🚧 Add deploy main function

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,15 @@
-const main = async () => { };
+const main = async () => {
+    const [deployer] = await hre.ethers.getSigners();
+    const accountBalance = await deployer.getBalance();
+
+    console.log('Deploying contracts with account: ', deployer.address);
+    console.log('Account balance: ', accountBalance.toString());
+
+    const Token = await hre.ethers.getContractFactory('TacoPortal');
+    const portal = await Token.deploy();
+
+    console.log('TacoPortal address: ', portal.address);
+};
 
 const runMain = async () => {
     try {
